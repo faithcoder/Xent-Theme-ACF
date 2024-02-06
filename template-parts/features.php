a98d3fd56d295496952b7ff328739d91f26ae6ca
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-02-06 23:22:27 +0600

    Feature List updated

--- template-parts/features.php
+++ template-parts/features.php
@@ -19,8 +19,8 @@
                   </div>
 
                   <?php
-                     $features_list = get_field('features_list', 'options');
-                     foreach($features_list as $feature) {
+                     $feature_list = get_field('feature_list', 'options');
+                     foreach($feature_list as $feature) {
                   ?>
                      <div class="services-2 w-100 d-flex">
                         <div class="icon d-flex align-items-center justify-content-center"><i class="fa <?php echo $feature['feature_list_icon'];?>"></i></div>
